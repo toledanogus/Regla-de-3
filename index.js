ad37f48e6e67661448e9ee73c68d3961dcb57a9d
toledanogus <toledanogu@gmail.com>
2023-09-30 10:21:59 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let x1, y1, x2, resultado, texto;
+
+
+calcular = ()=>{
+    x1=document.getElementById('x1').value;
+    console.log(x1);
+    y1=document.getElementById('y1').value;
+    console.log(y1);
+    x2=document.getElementById('x2').value;
+    console.log(x2);
+    resultado = ((x2*y1)/x1).toFixed(2)
+    texto = document.getElementById('result')
+    texto.innerText=resultado;
+    console.log(resultado);
+}
