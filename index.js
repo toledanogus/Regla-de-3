0598ef144640ff29755a522acfe309342952b223
toledanogus <toledanogu@gmail.com>
2023-10-01 09:09:11 -0600

    quinto

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ let x1, y1, x2, resultado, texto;
 
 calcular = ()=>{
     x1=document.getElementById('x1').value;
-    console.log(x1);
     y1=document.getElementById('y1').value;
-    console.log(y1);
     x2=document.getElementById('x2').value;
-    console.log(x2);
-    resultado = ((x2*y1)/x1).toFixed(2)
+    resultado = ((x2*y1)/x1)
+    if (resultado % 1 != 0) {
+        resultado = resultado.toFixed(2)
+    }
     if (isNaN(resultado)) {
         texto = document.getElementById('result')
         texto.innerText="0"
